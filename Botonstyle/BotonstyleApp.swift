6e1c084d79f74deacec229dab654be5190e9e320
RobbMolina <jose.molina@iest.edu.mx>
2025-09-08 18:53:03 -0600

    Initial Commit

---
+++ Botonstyle/BotonstyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotonstyleApp.swift
+//  Botonstyle
+//
+//  Created by win603 on 08/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BotonstyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

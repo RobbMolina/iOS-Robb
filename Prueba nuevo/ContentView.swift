3f32ccd87b899fe726f79e3e9812773570387caf
RobbMolina <jose.molina@iest.edu.mx>
2025-09-08 18:47:25 -0600

    Initial Commit

---
+++ Prueba nuevo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prueba nuevo
+//
+//  Created by win603 on 08/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
